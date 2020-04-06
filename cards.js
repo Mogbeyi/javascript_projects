5be67ddb0204acfc4904daba7e6a23c1f506fe8c
Mogbeyi <mogbeyiwenyinmi@gmail.com>
2020-04-06 16:19:31 +0100

    implement basic card functions

---
+++ cards.js
@@ -0,0 +1,41 @@
+const myDeck = {
+
+	deck: [],
+	drawnCards: [],
+	suits: ['hearts', 'diamonds', 'spades', 'clubs'],
+	values: '2,3,4,5,6,7,8,9,10,J,Q,K,A',
+
+	initializeDeck() {
+		const{deck, suits, values} = this;
+
+		for (let value of values.split(',')) {
+			for (let suit of suits) {
+				deck.push({value, suit})
+			}
+		}
+
+	},
+
+	drawCard() {
+		const card = this.deck.pop();
+		this.drawnCards.push(card);
+		return card;
+	},
+
+	drawMultipleCards(num) {
+		const multipleCards = [];
+		for (let i = 0; i < num; i++) {
+			multipleCards.push(this.drawnCard());
+		}
+
+		return multipleCards;
+	},
+
+	shuffle(deck) {
+  		let count = this.deck.length;
+  		while(count) {
+    		deck.push(deck.splice(Math.floor(Math.random() * count), 1)[0]);
+    		count -= 1;
+  		}
+  	}
+}
