1bda01b07b0e1272270129d306b8611504179492
Mogbeyi <mogbeyiwenyinmi@gmail.com>
2020-04-18 18:43:07 +0100

    fetch movie data and implement debounce

--- movie_battle/index.js
+++ movie_battle/index.js
@@ -1,1 +1,22 @@
-console.log('Hi there!');
+const fetchData = async (searchTerm) => {
+    const response = await axios.get('http://www.omdbapi.com/', {
+        params: {
+            apikey: "35acfb24",
+            s: searchTerm 
+        }
+    });
+    console.log(response.data);
+}
+
+const input = document.querySelector('input');
+
+let timeoutId;
+const onInput = (event) => {
+    if (timeoutId) clearTimeout(id);
+   timeoutId = setTimeout(() => {
+        const value = event.target.value;
+        fetchData(value);
+    }, 500)
+    
+}
+input.addEventListener('input', onInput);
