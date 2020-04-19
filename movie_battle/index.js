d81654a371d1996a33b01b373353f20b68c32369
Mogbeyi <mogbeyiwenyinmi@gmail.com>
2020-04-19 22:18:12 +0100

    fix broken images from api

--- movie_battle/index.js
+++ movie_battle/index.js
@@ -9,19 +9,36 @@ const fetchData = async (searchTerm) => {
     return response.data.Error ? [] : response.data.Search;
 }
 
+const autoComplete = document.querySelector('.autocomplete');
+autoComplete.innerHTML = `
+    <label><b>Search for a movie</b></label>
+    <input class="input">
+    <div class="dropdown">
+        <div class="dropdown-menu">
+            <div class="dropdown-content results"></div>
+        </div>
+    </div>
+`;
+
 const input = document.querySelector('input');
+const dropdown = document.querySelector('.dropdown');
+const resultsWrapper = document.querySelector('.results');
 
 const onInput = async (event) => {
     const movies = await fetchData(event.target.value);
     
+    resultsWrapper.innerHTML = '';
+    dropdown.classList.add('is-active');
     for (let movie of movies) {
-        const div = document.createElement('div');
+        const option = document.createElement('a');
+        const imgSrc = movie.Poster === "N/A" ? '' : movie.Poster;
+        option.classList.add('dropdown-item');
 
-        div.innerHTML = `
-            <img src="${movie.Poster}" />
-            <h1>${movie.Title}</h1>
+        option.innerHTML = `
+            <img src="${imgSrc}" />
+            ${movie.Title}
         `;
-        document.querySelector('#target').appendChild(div);
+        resultsWrapper.appendChild(option);
     }   
 }
 
